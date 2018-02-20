c78f74ef1a7ae73e49557a6d2a2cfb41e18164f8
vigneshwaran <mravigneshwaran@gmail.com>
2018-02-20 07:13:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send(createTemplate(article-one));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
