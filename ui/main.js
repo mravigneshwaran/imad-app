f770b268e676a14c72d813b21e6b53219d3116bd
vigneshwaran <mravigneshwaran@gmail.com>
2018-02-21 20:46:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,8 @@ submit.onclick = function () {
             //Take some action
             if(request.status === 200){
                  //Capture a list of names and render it as a list
-                    var names = ['name1', 'name2', 'name3','name4'];
+                    var names = request.responseText;
+                    names = JSON.parse(names);
                     var list = '';
                     for(var i=0; i<names.length; i++){
                         list += '<li>' + names[i] + '</li>';
@@ -56,7 +57,7 @@ submit.onclick = function () {
     
     //Make the request
     
-    request.open('GET','http://mravigneshwaran.imad.hasura-app.io/counter',true);
+    request.open('GET','http://mravigneshwaran.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
     
