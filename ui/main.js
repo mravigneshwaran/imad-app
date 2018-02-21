16b3f6edf5ae96315b711a162337849b0f7fb2f9
vigneshwaran <mravigneshwaran@gmail.com>
2018-02-21 20:54:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,7 @@ button.onclick = function(){
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //Create a request
@@ -56,7 +55,8 @@ submit.onclick = function () {
     };
     
     //Make the request
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;  
     request.open('GET','http://mravigneshwaran.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
