ca2c9b9d962bfecb6da18d23d182befaa57895de
vigneshwaran <mravigneshwaran@gmail.com>
2018-02-21 09:35:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,14 +32,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //Make a request to the server and send the name
+    //Create a request
+    var request = new XMLHttpRequest();
     
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3','name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                 //Capture a list of names and render it as a list
+                    var names = ['name1', 'name2', 'name3','name4'];
+                    var list = '';
+                    for(var i=0; i<names.length; i++){
+                        list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;             
+            }
+        }
+        // Not yet done
+    };
+    
+    //Make the request
+    
+    request.open('GET','http://mravigneshwaran.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
+    
 };
