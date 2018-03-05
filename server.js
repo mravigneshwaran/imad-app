7c978231c5b5813fa57c7d560a903965317fc9cc
vigneshwaran <mravigneshwaran@gmail.com>
2018-03-05 21:48:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/articles/:articalName', function(req,res){
     //extract article name
     var articalName = req.params.articalName;
     
-    pool.query("select * from article where title= "+req.params.articleName, function(err,result){
+    pool.query("select * from article where title= "+req.params.articleName + "'", function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
